a6180202f7e3d4ba18252c86403e2b1fece99943
Baggette <rishijsarwan@gmail.com>
2025-11-05 10:32:57 -0600

    updates

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ except FileNotFoundError:
 f = open("./data.json", "r")
 
 budgetData = json.load(f)
-mainMenuMSG = """\nPersonal Budget Tracker
+mainMenuMSG = """\nPersonal Budget Tracker\n
 1. Income and Expense Tracking
 2. Budget management 
 3. View all transactions
@@ -17,15 +17,25 @@ mainMenuMSG = """\nPersonal Budget Tracker
 Select an option: """
 #this is a change
    
-addIncomeMSG = """Budget Management 
+addIncomeMSG = """\nBudget Management\n
 1. Add income
 2. Add expense 
 3. Exit
 
 Select an option: """
 
+expenseCategoryMSG = """\nExpense Category\n
+1. Food
+2. Transportation
+3. Entertainment
+4. Bills
+5. Other
+
+Select an option: """
+
+categories = ["food","transportation", "Entertainment", "bills", "other"]
 def addIncome():
-    option = input(addIncomeMSG)
+    option = input(addIncomeMSG).strip()
     if option == "1":
         description = input("Income Description: ")
         try:
@@ -33,7 +43,18 @@ def addIncome():
             budgetData["income"].append({"description" : description, "income_amount" : amount})
             print(budgetData)
         except ValueError:  
-            input("You did not enter a valid number! \nPress enter to continue")
+            input("You did not enter a valid number! \nPress enter to continue...")
+            addIncome()
+    elif option == "2":
+        try:
+            category = int(input(expenseCategoryMSG))
+            if category <= 0 or category > 5:
+                raise ValueError
+            else:
+                description = input("Expense Description: ")
+                amount = float(input(""))
+        except ValueError:
+            input("You did not enter a valid number!\nPress enter to continue...")
             addIncome()
 
 
